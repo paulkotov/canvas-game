3eea907be59182ec60899e2511a7fa64cedda11c
Paul Kotov <paulkotov1@gmail.com>
2025-08-03 17:11:05 +0300

    code: add class Controls

--- src/core.js
+++ src/core.js
@@ -267,3 +267,43 @@ class Player {
     }
   }
 }
+
+class Controls {
+  codes = { 37: 'left', 39: 'right', 38: 'forward', 40: 'backward' };
+  states = { left: false, right: false, forward: false, backward: false };
+
+  constructor() {
+    document.addEventListener('keydown', this.onKey.bind(this, true), false);
+    document.addEventListener('keyup', this.onKey.bind(this, false), false);
+    document.addEventListener('touchstart', this.onTouch.bind(this), false);
+    document.addEventListener('touchmove', this.onTouch.bind(this), false);
+    document.addEventListener('touchend', this.onTouchEnd.bind(this), false);
+  }
+
+  onTouch = (e) => {
+    const t = e.touches[0];
+    this.onTouchEnd(e);
+    if (t.pageY < window.innerHeight * 0.5) {
+      this.onKey(true, { keyCode: 38 });
+    } else if (t.pageX < window.innerWidth * 0.5) {
+      this.onKey(true, { keyCode: 37 });
+    } else if (t.pageX > window.innerWidth * 0.5) {
+      this.onKey(true, { keyCode: 39 });
+    }
+  };
+
+  onTouchEnd = (e) => {
+    this.states = { left: false, right: false, forward: false, backward: false };
+    e.preventDefault();
+    e.stopPropagation();
+  };
+
+  onKey = (val, e) => {
+    const state = this.codes[e.keyCode];
+    if (typeof state === 'undefined') return;
+    this.states[state] = val;
+    if (e.preventDefault) e.preventDefault();
+    if (e.stopPropagation) e.stopPropagation();
+  }
+}
+
